237bee7f81f0b3f88c959560d767927bc80ed5f6
Filipe Azevedo <pasnox@gmail.com>
2007-09-05 13:10:55 +0000

    no change

--- fresh/widgets/pDockToolBar.cpp
+++ fresh/widgets/pDockToolBar.cpp
@@ -19,7 +19,7 @@
 #include <QMainWindow>
 
 pDockToolBar::pDockToolBar( pDockToolBarManager* t, Qt::Orientation o )
-	: mManager( t ), mUniqueId( 0 ), mExclusive( true )
+	: QToolBar(), mManager( t ), mUniqueId( 0 ), mExclusive( true )
 {
 	// need docktoolbar manager
 	Q_ASSERT( t != 0 );
