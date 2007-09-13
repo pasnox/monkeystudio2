76e347e0bc283f23e6ad402789c8f6c4c94f64c3
Filipe Azevedo <pasnox@gmail.com>
2007-09-13 13:50:09 +0000

    apply good formatting

--- monkey/src/workspace/pFileManager.cpp
+++ monkey/src/workspace/pFileManager.cpp
@@ -48,8 +48,8 @@ pAbstractChild* pFileManager::openFile( const QString& s, const QPoint& p )
 		connect( c, SIGNAL( fileClosed( const QString& ) ), this, SIGNAL( fileClosed( const QString& ) ) );
 		// update file menu
 		connect( c, SIGNAL( modifiedChanged( bool ) ), pMenuBar::instance()->action( "mFile/mSave/aCurrent" ), SLOT( setEnabled( bool ) ) );
-        //update tab icon
-    connect( c, SIGNAL( modifiedChanged( bool ) ), pWorkspace::instance(), SLOT( markTabAsNotSaved(bool) ) );
+		//update tab icon
+		connect( c, SIGNAL( modifiedChanged( bool ) ), pWorkspace::instance(), SLOT( markTabAsNotSaved( bool ) ) );
 		// update edit menu
 		connect( c, SIGNAL( undoAvailableChanged( bool ) ), pMenuBar::instance()->action( "mEdit/aUndo" ), SLOT( setEnabled( bool ) ) );
 		connect( c, SIGNAL( redoAvailableChanged( bool ) ), pMenuBar::instance()->action( "mEdit/aRedo" ), SLOT( setEnabled( bool ) ) );
