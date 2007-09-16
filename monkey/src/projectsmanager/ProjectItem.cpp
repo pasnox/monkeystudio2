a36b3daf3cc53158575473c2b365d781dc8c4908
Filipe Azevedo <pasnox@gmail.com>
2007-09-16 09:03:03 +0000

    Make abstract and move all other members to qmake plugins

--- monkey/src/projectsmanager/ProjectItem.cpp
+++ monkey/src/projectsmanager/ProjectItem.cpp
@@ -13,7 +13,6 @@
 **
 ****************************************************************************/
 #include "ProjectItem.h"
-#include "pMonkeyStudio.h"
 
 #include <QFileInfo>
 #include <QDir>
@@ -21,6 +20,8 @@
 
 ProjectItem::ProjectItem( ProjectsModel::NodeType t, ProjectItem* i )
 {
+	// clear buffer
+	mBuffer.clear();
 	// set type
 	setType( t );
 	// set readonly
@@ -29,9 +30,6 @@ ProjectItem::ProjectItem( ProjectsModel::NodeType t, ProjectItem* i )
 	if ( i )
 		i->appendRow( this );
 }
-
-ProjectItem::~ProjectItem()
-{}
 
 void ProjectItem::setData( const QVariant& v, int r )
 {
@@ -125,56 +123,24 @@ void ProjectItem::setReadOnly( bool b )
 bool ProjectItem::getReadOnly() const
 { return data( ProjectsModel::ReadOnlyRole ).toBool(); }
 
-QString ProjectItem::getIndent() const
-{
-	// if scope end, need take parent indent
-	if ( const_cast<ProjectItem*>( this )->getType() == ProjectsModel::ScopeEndType )
-		return parent()->getIndent();
-	// if first value of a variable, no indent
-	if ( const_cast<ProjectItem*>( this )->getType() == ProjectsModel::ValueType && isFirst() )
-		return QString();
-	// if not value, and parent is nestedscope, no indent
-	if ( parent() && parent()->getType() == ProjectsModel::NestedScopeType )
-		return QString();
-	// default indent
-	int i = -1;
-	// count parent
-	ProjectItem* p = project();
-	ProjectItem* it = const_cast<ProjectItem*>( this );
-	while ( it && ( it = it->parent() ) )
-		if ( it->project() == p )
-			i++;
-	// return indent
-	return QString().fill( '\t', i );
-}
-
-QString ProjectItem::getEol() const
-{
-	switch ( pMonkeyStudio::eolMode() )
-	{
-		case QsciScintilla::EolWindows:
-			return QString( "\r\n" );
-			break;
-		case QsciScintilla::EolUnix:
-			return QString( "\r" );
-			break;
-		case QsciScintilla::EolMac:
-			return QString( "\n" );
-			break;
-	}
-}
-
-bool ProjectItem::isFirst() const
-{ return parent() ? parent()->child( 0, 0 ) == this : true; }
-
-bool ProjectItem::isLast() const
-{ return parent() ? parent()->rowCount() -1 == row() : true; }
-
 ProjectsModel* ProjectItem::model() const
 { return reinterpret_cast<ProjectsModel*>( QStandardItem::model() ); }
 
 ProjectItem* ProjectItem::parent() const
 { return reinterpret_cast<ProjectItem*>( QStandardItem::parent() ); }
+
+int ProjectItem::parentCount() const
+{
+	ProjectItem* p = project();
+	ProjectItem* it = const_cast<ProjectItem*>( this );
+	int i = 0;
+	while ( it && it != p )
+	{
+		i++;
+		it = it->parent();
+	}
+	return i;
+}
 
 ProjectItem* ProjectItem::child( int i, int j ) const
 { return reinterpret_cast<ProjectItem*>( QStandardItem::child( i, j ) ); }
@@ -196,102 +162,12 @@ ProjectItemList ProjectItem::children( bool r, bool s ) const
 	return l;
 }
 
-ProjectItem* ProjectItem::clone( ProjectsModel::NodeType t, ProjectItem* p ) const
-{ return new ProjectItem( t, p ); }
-
-void ProjectItem::appendRow( ProjectItem* i )
-{ insertRow( rowCount(), i ); }
-
-void ProjectItem::insertRow( int r, ProjectItem* i )
-{
-	// check scope & function item for not be able to append items after scope end
-	ProjectItem* it = reinterpret_cast<ProjectItem*>( i );
-	if ( it && ( getType() == ProjectsModel::NestedScopeType || getType() == ProjectsModel::ScopeType || getType() == ProjectsModel::FunctionType ) && it->getType() != ProjectsModel::ScopeEndType )
-		r--;
-	// default insert
-	QStandardItem::insertRow( r, i );
-}
-
-bool ProjectItem::swapRow( int i, int j )
-{
-	// temp row list
-	QList<QStandardItem*> ii;
-	QList<QStandardItem*> ij;
-	// if get the rows
-	if ( !( ii = takeRow( i ) ).isEmpty() && !( ij = takeRow( j ) ).isEmpty() )
-	{
-		// reinsert them
-		QStandardItem::insertRow( i, ij );
-		QStandardItem::insertRow( j, ii );
-	}
-	// return true if they are not empty
-	return !ii.isEmpty() && !ij.isEmpty();
-}
-
-bool ProjectItem::moveRowUp( int i )
-{
-	// we can't move up the scope end item
-	ProjectItem* it = child( i, 0 );
-	if ( !it || ( it && it->getType() == ProjectsModel::ScopeEndType ) )
-		return false;
-	// if valid, move it
-	if ( i > 0 )
-		QStandardItem::insertRow( i -1, takeRow( i ) );
-	return i > 0;
-}
-
-bool ProjectItem::moveRowDown( int i )
-{
-	// if in a scope we can t go to very last item as it s the scope end
-	ProjectItem* it = child( i +1, 0 );
-	if ( !it || ( it && it->getType() == ProjectsModel::ScopeEndType ) )
-		return false;
-	// if valid, move it
-	if ( i < rowCount() -1 )
-		QStandardItem::insertRow( i +1, takeRow( i ) );
-	return i < rowCount() -1;
-}
-
-bool ProjectItem::moveUp()
-{ return parent() ? parent()->moveRowUp( row() ) : false; }
-
-bool ProjectItem::moveDown()
-{ return parent()  ? parent()->moveRowDown( row() ) : false; }
-
-#include <QDebug>
-void ProjectItem::remove()
-{
-	qWarning( qPrintable( QString( "try removing: %1 (%2), %3-%4" ).arg( getValue() ).arg( getType() ).arg( row() ).arg( column() ) ) );
-	if ( model() )
-	{
-		qDebug() << (parent() ? parent()->index() : QModelIndex());
-		model()->removeRow( row(), parent() ? parent()->index() : QModelIndex() );
-	}
-	else if ( parent() )
-		parent()->removeRow( row() );
-	else
-		delete this;
-}
-
 ProjectItem* ProjectItem::project() const
 {
-	// get project item
 	ProjectItem* it = const_cast<ProjectItem*>( this );
-	// check reucursively parent if needed
 	while ( it && it->getType() != ProjectsModel::ProjectType )
 		it = it->parent();
-	// return item
 	return it;
-}
-
-ProjectItemList ProjectItem::projectItems() const
-{
-	ProjectItem* p = project();
-	ProjectItemList l;
-	foreach ( ProjectItem* it, p->children( true ) )
-		if ( it->project() == p )
-			l << it;
-	return l;
 }
 
 ProjectItem* ProjectItem::parentProject() const
@@ -303,11 +179,14 @@ ProjectItem* ProjectItem::parentProject() const
 ProjectItemList ProjectItem::childrenProjects( bool b ) const
 {
 	ProjectItemList l;
-	foreach ( ProjectItem* it, children( b) )
+	foreach ( ProjectItem* it, children( b ) )
 		if ( it->getType() == ProjectsModel::ProjectType )
 			l << it;
 	return l;
 }
+
+ProjectItemList ProjectItem::projectItems() const
+{ return project()->children( true, true ); }
 
 ProjectItemList ProjectItem::projectScopes() const
 {
@@ -320,264 +199,26 @@ ProjectItemList ProjectItem::projectScopes() const
 
 QStringList ProjectItem::projectScopesList() const
 {
-	QStringList l( "" ); // root scope
-	foreach ( ProjectItem* it, projectItems() )
+	QStringList l( "" );
+	foreach ( ProjectItem* it, projectScopes() )
 	{
-		if ( it->getType() == ProjectsModel::ScopeType || it->getType() == ProjectsModel::NestedScopeType )
-		{
-			const QString s = it->scope();
-			if ( !l.contains( s ) )
-				l << s;
-		}
+		const QString s = it->scope();
+		if ( !l.contains( s ) )
+			l << s;
 	}
 	return l;
 }
-
-ProjectItemList ProjectItem::match( ProjectItem* i, int r, const QVariant& v ) const
-{
-	ProjectItemList l;
-	const QString s = v.toString().toLower();
-	foreach ( ProjectItem* it, i->projectItems() )
-		if ( it->getValue().toLower() == s || s == "*" )
-			l << it;
-	return l;
-}
-
-QString ProjectItem::checkScope( const QString& s ) const
-{
-	// remove trailing : as it s the default operator
-	QString ss = s.trimmed();
-	if ( ss.endsWith( ':' ) )
-		ss.chop( 1 );
-	return ss;
-}
-
-bool ProjectItem::isEqualScope( const QString& s1, const QString& s2 ) const
-{
-	if ( s2 == "*" )
-		return true;
-	QString ss1( s1.toLower() ), ss2( s2.toLower() );
-	ss1.replace( '/', ':' );
-	ss2.replace( '/', ':' );
-	return checkScope( ss1 ) == checkScope( ss2 );
-}
-
-QString ProjectItem::scope() const
-{
-	QString s;
-	ProjectItem* j = const_cast<ProjectItem*>( this );
-	ProjectItem* p = project();
-	while ( j && j != p )
-	{
-		if ( j->getType() == ProjectsModel::ScopeType || j->getType() == ProjectsModel::NestedScopeType )
-			s.prepend( QString( "%1%2" ).arg( j->getValue(), j->getOperator().isEmpty() ? ":" : j->getOperator() ) );
-		j = j->parent();
-	}
-	s.chop( 1 ); // remove trailing operator
-	return s;
-}
-
-ProjectItemList ProjectItem::getItemList( ProjectsModel::NodeType t, const QString& v, const QString& o, const QString& s ) const
-{
-	// temp list
-	ProjectItemList l;
-	foreach ( ProjectItem* it, match( project(), ProjectsModel::ValueRole, v ) )
-		// check same scope
-		if ( isEqualScope( it->scope(), s ) )
-			// check operator
-			if ( ( !o.isEmpty() && it->getOperator() == o ) || o.isEmpty() )
-				// check type
-				if ( it->getType() == t )
-					// add to list
-					l << it;
-	// return list
-	return l;
-}
-
-ProjectItem* ProjectItem::getItemScope( const QString& s, bool c ) const
-{
-	// get project item
-	ProjectItem* sItem = project();
-	// it there is scope
-	if ( !s.trimmed().isEmpty() )
-	{
-		// get item scope and separeted operator
-		static QRegExp r( "([^:|]+)(\\:|\\|)?" );
-		int pos = 0;
-		QString cScope = checkScope( s );
-		// create scope is there are not existing
-		while ( ( pos = r.indexIn( cScope, pos ) ) != -1 )
-		{
-			QStringList l = r.capturedTexts();
-			QString s1 = l.at( 1 );
-			QString s2 = l.at( 2 );
-			QString sScope = cScope.left( pos ).append( s1 );
-			// search existing scope
-			ProjectItem* fItem = ( sItem->getItemList( ProjectsModel::ScopeType, s1, s2, sScope ) << sItem->getItemList( ProjectsModel::NestedScopeType, s1, s2, sScope ) ).value( 0 );
-			// create scope
-			if ( ( !fItem || fItem->project() != sItem ) )
-			{
-				if ( c )
-				{
-					sItem = clone( l.at( 2 ).trimmed().isEmpty() ? ProjectsModel::ScopeType : ProjectsModel::NestedScopeType, sItem );
-					sItem->setValue( s1 );
-					sItem->setOperator( s2 );
-					(void) clone( ProjectsModel::ScopeEndType, sItem );
-				}
-				else
-					return sItem;
-			}
-			else
-				sItem = fItem;
-			pos += r.matchedLength();
-		}
-		// if scope is nested, made it ScopeType
-		if ( sItem && sItem->getType() == ProjectsModel::NestedScopeType )
-		{
-			sItem->setType( ProjectsModel::ScopeType );
-			sItem->setOperator( QString::null );
-		}
-	}
-	// return scope item
-	return sItem;
-}
-
-ProjectItemList ProjectItem::getItemListValues( const QString& v, const QString& o, const QString& s ) const
-{
-	ProjectItemList l = getItemList( ProjectsModel::VariableType, v, o, s );
-	ProjectItemList ll;
-	foreach ( ProjectItem* it, l )
-		for ( int i = 0; i < it->rowCount(); i++ )
-			if ( it->child( i, 0 )->getType() == ProjectsModel::ValueType )
-				ll << it->child( i, 0 );
-	return ll;
-}
-
-ProjectItem* ProjectItem::getItemVariable( const QString& v, const QString& o, const QString& s ) const
-{ return getItemList( ProjectsModel::VariableType, v, o, s ).value ( 0 ); }
-
-QStringList ProjectItem::getListValues( const QString& v, const QString& o, const QString& s ) const
-{
-	QStringList l;
-	foreach ( ProjectItem* it, getItemList( ProjectsModel::VariableType, v, o, s ) )
-		for ( int i = 0; i < it->rowCount(); i++ )
-			if ( it->child( i, 0 )->getType() == ProjectsModel::ValueType )
-				l << it->child( i, 0 )->getValue();
-	return l;
-}
-
-QString ProjectItem::getStringValues( const QString& v, const QString& o, const QString& s ) const
-{ return getListValues( v, o, s ).join( " " ); }
-
-void ProjectItem::setListValues( const QStringList& vl, const QString& v, const QString& o, const QString& s )
-{
-	ProjectItem* p = project();
-	ProjectItem* it = getItemVariable( v, o, s );
-	// create index if it not exists
-	if ( !it )
-	{
-		// if nothing to add, return
-		if ( vl.isEmpty() )
-			return;
-		// create variable
-		it = clone( ProjectsModel::VariableType, getItemScope( s, true ) );
-		it->setValue( v );
-		it->setOperator( o );
-	}
-	// set values
-	QStringList l = vl;
-	// check all child of variable and remove from model/list unneeded index/value
-	foreach ( ProjectItem* c, it->children( false, true ) )
-	{
-		if ( c->getType() == ProjectsModel::ValueType )
-		{
-			if ( l.contains( c->getValue() ) )
-				l.removeAll( c->getValue() );
-			else
-				c->remove();
-		}
-	}
-	// add require index
-	if ( it )
-	{
-		foreach ( QString e, l )
-		{
-			if ( !e.isEmpty() )
-			{
-				ProjectItem* cItem = clone( ProjectsModel::ValueType, it );
-				cItem->setValue( e );
-			}
-		}
-		// if variable is empty, remove it
-		if ( !it->rowCount() )
-			it->remove();
-	}
-}
-
-void ProjectItem::setStringValues( const QString& val, const QString& v, const QString& o, const QString& s )
-{ setListValues( val.isEmpty() ? QStringList() : QStringList( val ), v, o, s ); }
-
-void ProjectItem::addListValues( const QStringList& vl, const QString& v, const QString& o, const QString& s )
-{
-	ProjectItem* p = project();
-	ProjectItem* it = getItemVariable( v, o, s );
-	// create index if it not exists
-	if ( !it )
-	{
-		// if nothing to add, return
-		if ( vl.isEmpty() )
-			return;
-		// create variable
-		it = clone( ProjectsModel::VariableType, getItemScope( s, true ) );
-		it->setValue( v );
-		it->setOperator( o );
-	}
-	// add values
-	QStringList l = vl;
-	// check all values of variable and remove from list already existing values
-	for ( int j = it->rowCount() -1; j > -1; j-- )
-	{
-		ProjectItem* c = it->child( j, 0 );
-		if ( c->getType() == ProjectsModel::ValueType )
-			if ( l.contains( c->getValue() ) )
-				l.removeAll( c->getValue() );
-	}
-	// add require values
-	if ( it )
-	{
-		foreach ( QString e, l )
-		{
-			if ( !e.isEmpty() )
-			{
-				ProjectItem* cItem = clone( ProjectsModel::ValueType, it );
-				cItem->setValue( e );
-			}
-		}
-	}
-}
-
-void ProjectItem::addStringValues( const QString& val, const QString& v, const QString& o, const QString& s )
-{ addListValues( QStringList( val ), v, o, s ); }
 
 QString ProjectItem::canonicalFilePath() const
-{
-	// get project item
-	ProjectItem* it = project();
-	// return project absolutepath
-	return it ? it->getFilePath() : QString();
-}
+{ return project()->getFilePath(); }
 
 QString ProjectItem::canonicalFilePath( const QString& s ) const
 {
-	// file path
 	QString fp = s;
-	// check if relative or absolute
 	if ( QFileInfo( fp ).isRelative() )
 		fp = canonicalPath().append( "/" ).append( fp );
-	// return canonical file path if file exists, exists
 	if ( QFile::exists( fp ) )
 		return QFileInfo( fp ).canonicalFilePath();
-	// else return absolute file path, may be not exists
 	return QFileInfo( fp ).absoluteFilePath();
 }
 
@@ -598,3 +239,37 @@ QString ProjectItem::completeBaseName( const QString& s )
 
 QString ProjectItem::name() const
 { return QFileInfo( canonicalFilePath() ).completeBaseName(); }
+
+ProjectItemList ProjectItem::match( int r, const QVariant& v ) const
+{
+	ProjectItemList l;
+	const QString s = v.toString();
+	foreach ( ProjectItem* it, projectItems() )
+		if ( it->getValue() == s || s == "*" )
+			l << it;
+	return l;
+}
+
+ProjectItemList ProjectItem::getItemList( ProjectsModel::NodeType t, const QString& v, const QString& o, const QString& s ) const
+{
+	ProjectItemList l;
+	foreach ( ProjectItem* it, match( ProjectsModel::ValueRole, v ) )
+		if ( isEqualScope( it->scope(), s ) )
+			if ( ( !o.isEmpty() && it->getOperator() == o ) || o.isEmpty() )
+				if ( it->getType() == t )
+					l << it;
+	return l;
+}
+
+ProjectItemList ProjectItem::getItemListValues( const QString& v, const QString& o, const QString& s ) const
+{
+	ProjectItemList l;
+	foreach ( ProjectItem* it, getItemList( ProjectsModel::VariableType, v, o, s ) )
+		for ( int i = 0; i < it->rowCount(); i++ )
+			if ( it->child( i, 0 )->getType() == ProjectsModel::ValueType )
+				l << it->child( i, 0 );
+	return l;
+}
+
+ProjectItem* ProjectItem::getItemVariable( const QString& v, const QString& o, const QString& s ) const
+{ return getItemList( ProjectsModel::VariableType, v, o, s ).value ( 0 ); }
--- monkey/src/projectsmanager/ProjectItem.h
+++ monkey/src/projectsmanager/ProjectItem.h
@@ -33,11 +33,9 @@ class Q_MONKEY_EXPORT ProjectItem : /*public QObject,*/ public QStandardItem
 	
 public:
 	ProjectItem( ProjectsModel::NodeType = ProjectsModel::ProjectType, ProjectItem* = 0 );
-virtual ~ProjectItem();
-
-	// the plugin this item come from
-	virtual QString pluginName() const { return QString::null; }
 	
+	// plugin name
+	virtual QString pluginName() const = 0;
 	// set data
 	virtual void setData( const QVariant&, int = Qt::UserRole +1 );
 	// type of the item
@@ -71,83 +69,55 @@ virtual ~ProjectItem();
 	virtual void setReadOnly( bool );
 	virtual bool getReadOnly() const;
 	// item indent
-	virtual QString getIndent() const;
+	virtual QString getIndent() const = 0;
 	// item eol
-	virtual QString getEol() const;
+	virtual QString getEol() const = 0;
 	// tell if item is the first child of its parent
-	virtual bool isFirst() const;
+	virtual bool isFirst() const = 0;
 	// tell if item is the last child of its parent
-	virtual bool isLast() const;
+	virtual bool isLast() const = 0;
+	// scope of item
+	virtual QString scope() const = 0;
+	// check scope
+	virtual QString checkScope( const QString& ) const = 0;
+	// check equals scope
+	virtual bool isEqualScope( const QString&, const QString& ) const = 0;
 	
 	// item model
 	virtual ProjectsModel* model() const;
 	// item parent
 	virtual ProjectItem* parent() const;
+	// parents number
+	virtual int parentCount() const;
 	// child item
 	virtual ProjectItem* child( int, int = 0 ) const;
 	// get children, recursively according to bool and same project according to bool
 	virtual ProjectItemList children( bool = false, bool = false ) const;
-	// return a new item of type of caller
-	virtual ProjectItem* clone( ProjectsModel::NodeType = ProjectsModel::ProjectType, ProjectItem* = 0 ) const;
 	// append item
-	virtual void appendRow( ProjectItem* );
+	virtual void appendRow( ProjectItem* ) = 0;
 	// insert item
-	virtual void insertRow( int, ProjectItem* );
+	virtual void insertRow( int, ProjectItem* ) = 0;
 	// moving item
-	virtual bool swapRow( int, int );
-	virtual bool moveRowUp( int );
-	virtual bool moveRowDown( int );
-	virtual bool moveUp();
-	virtual bool moveDown();
+	virtual bool swapRow( int, int ) = 0;
+	virtual bool moveRowUp( int ) = 0;
+	virtual bool moveRowDown( int ) = 0;
+	virtual bool moveUp() = 0;
+	virtual bool moveDown() = 0;
 	// remove itself
-	virtual void remove();
+	virtual void remove() = 0;
 	
 	// the project item
 	virtual ProjectItem* project() const;
-	// get all project items
-	virtual ProjectItemList projectItems() const;
 	// parent project in case it s a subproject
 	virtual ProjectItem* parentProject() const;
 	// get direct child project, or all according to bool
 	virtual ProjectItemList childrenProjects( bool = true ) const;
+	// get all project items
+	virtual ProjectItemList projectItems() const;
 	// get all scope from project
 	virtual ProjectItemList projectScopes() const;
 	// get all unique scope list from project
 	virtual QStringList projectScopesList() const;
-	// get last scope/nestedscope of this item
-	virtual ProjectItem* lastScope() const { return 0; }
-	// get very last project scope item
-	virtual ProjectItem* lastProjectScope() const { return 0; }
-	
-	// get index list
-	virtual ProjectItemList match( ProjectItem*, int, const QVariant& ) const;
-	// check scope
-	virtual QString checkScope( const QString& ) const;
-	// check equals scope
-	virtual bool isEqualScope( const QString&, const QString& ) const;
-	// scope of item
-	virtual QString scope() const;
-	// get all items matching
-	virtual ProjectItemList getItemList( ProjectsModel::NodeType, const QString&, const QString&, const QString& ) const;
-	// get item scope, creating it if needed
-	virtual ProjectItem* getItemScope( const QString&, bool ) const;
-	// get all variable content as modelindex list for project index
-	virtual ProjectItemList getItemListValues( const QString&, const QString&, const QString& ) const;
-	// get a variable index
-	virtual ProjectItem* getItemVariable( const QString&, const QString&, const QString& ) const;
-	// get variable content as stringlist for project index
-	virtual QStringList getListValues( const QString&, const QString& = "=", const QString& = QString::null ) const;
-	// get variable content as string for project index
-	virtual QString getStringValues( const QString&, const QString& = "=", const QString& = QString::null ) const;
-	// set variable content as stringlist for project index
-	virtual void setListValues( const QStringList&, const QString&, const QString& = "=", const QString& = QString::null );
-	// get variable content as string for project index
-	virtual void setStringValues( const QString&, const QString&, const QString& = "=", const QString& = QString::null );
-	// add variable content as stringlist for project index
-	virtual void addListValues( const QStringList&, const QString&, const QString& = "=", const QString& = QString::null );
-	// add variable content as string for project index
-	virtual void addStringValues( const QString&, const QString&, const QString& = "=", const QString& = QString::null );
-	
 	// canonical project filepath
 	virtual QString canonicalFilePath() const;
 	// canonical file path according to project path
@@ -164,19 +134,45 @@ virtual ~ProjectItem();
 	virtual QString completeBaseName( const QString& );
 	// name of project
 	virtual QString name() const;
-	
 	// close the project
-	virtual void close() {}
+	virtual void close() = 0;
 	// save project, asking user according to bool
-	virtual void save( bool = true ) {}
+	virtual void save( bool = true ) = 0;
 	// save project including all children projects
-	virtual void saveAll( bool = true ) {}
+	virtual void saveAll( bool = true ) = 0;
+	// show the content of items 
+	virtual void debug() = 0;
+	
+	
+	// get index list
+	virtual ProjectItemList match( int, const QVariant& ) const;
+	// get all items matching
+	virtual ProjectItemList getItemList( ProjectsModel::NodeType, const QString&, const QString&, const QString& ) const;
+	// get item scope, creating it if needed
+	virtual ProjectItem* getItemScope( const QString&, bool ) const = 0;
+	// get all variable content as modelindex list for project index
+	virtual ProjectItemList getItemListValues( const QString&, const QString&, const QString& ) const;
+	// get a variable index
+	virtual ProjectItem* getItemVariable( const QString&, const QString&, const QString& ) const;
+	
+	// get variable content as stringlist for project index
+	virtual QStringList getListValues( const QString&, const QString& = "=", const QString& = QString::null ) const = 0;
+	// get variable content as string for project index
+	virtual QString getStringValues( const QString&, const QString& = "=", const QString& = QString::null ) const = 0;
+	// set variable content as stringlist for project index
+	virtual void setListValues( const QStringList&, const QString&, const QString& = "=", const QString& = QString::null ) = 0;
+	// get variable content as string for project index
+	virtual void setStringValues( const QString&, const QString&, const QString& = "=", const QString& = QString::null ) = 0;
+	// add variable content as stringlist for project index
+	virtual void addListValues( const QStringList&, const QString&, const QString& = "=", const QString& = QString::null ) = 0;
+	// add variable content as string for project index
+	virtual void addStringValues( const QString&, const QString&, const QString& = "=", const QString& = QString::null ) = 0;
 	
 protected:
 	QString mBuffer;
-	virtual void redoLayout( ProjectItem* = 0 ) {}
-	virtual void writeProject() {}
-	virtual void writeItem( ProjectItem* ) {}
+	virtual void redoLayout( ProjectItem* = 0 ) = 0;
+	virtual void writeProject() = 0;
+	virtual void writeItem( ProjectItem* ) = 0;
 
 };
 
