4a7141d1a43bd0e92b685240cedcd47b8e2677e5
Filipe Azevedo <pasnox@gmail.com>
2007-10-13 19:26:46 +0000

    dialog for adding existing files to project

---
+++ monkey/src/projectsmanager/ui/UIAddExistingFiles.cpp
@@ -0,0 +1,62 @@
+/****************************************************************************
+**
+** 		Created using Monkey Studio v1.7.0
+** Author    : Azevedo Filipe aka Nox P@sNox <pasnox@gmail.com>
+** Project   : UIAddExistingFiles
+** FileName  : UIAddExistingFiles.cpp
+** Date      : 2007-10-13T20:00:33
+** License   : GPL
+** Comment   : Your comment here
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+#include "UIAddExistingFiles.h"
+
+#include <QGridLayout>
+#include <QFrame>
+#include <QHBoxLayout>
+
+UIAddExistingFiles::UIAddExistingFiles( ProjectsProxy* p, ProjectItem* it, const QStringList& l, QWidget* w )
+	: QFileDialog( w, tr( "Choose file(s)/project(s) to add to your project" ), it->canonicalPath() ), mProxy( p ), mItem( it )
+{
+	Q_ASSERT( mProxy );
+	Q_ASSERT( mItem );
+	// set file/accept mode
+	setAcceptMode( QFileDialog::AcceptOpen );
+	setFileMode( QFileDialog::ExistingFiles );
+	// create frame for comboboxes
+	QFrame* f = new QFrame;
+	// add layout to frame
+	QHBoxLayout* hl = new QHBoxLayout( f );
+	hl->setMargin( 0 );
+	hl->setSpacing( 3 );
+	// add projects/scopes groupbox
+	QGroupBox* gb1 = new QGroupBox;
+	gb1->setTitle( tr( "Scopes" ) );
+	// set groupbox layout
+	QHBoxLayout* hl1 = new QHBoxLayout( gb1 );
+	hl1->setMargin( 0 );
+	hl1->setSpacing( 0 );
+	// create projects/scopes treecombobox
+	tcbProjects = new pTreeComboBox;
+	tcbProjects->setModel( mProxy );
+	tcbProjects->setCurrentIndex( mProxy->mapFromSource( mItem->index() ) );
+	hl1->addWidget( tcbProjects );
+	// add operators groupbox
+	QGroupBox* gb2 = new QGroupBox;
+	gb2->setSizePolicy( QSizePolicy( QSizePolicy::Maximum, QSizePolicy::Preferred ) );
+	gb2->setTitle( tr( "Operators" ) );
+	// set groupbox layout
+	QHBoxLayout* hl2 = new QHBoxLayout( gb2 );
+	// create operators combobox
+	cbOperators = new QComboBox;
+	cbOperators->addItems( l );
+	hl2->addWidget( cbOperators );
+	// add groupbox to layout
+	hl->addWidget( gb1 );
+	hl->addWidget( gb2 );
+	// add frame to layout
+	qobject_cast<QGridLayout*>( layout() )->addWidget( f, 4, 0, 1, 3 );
+}
---
+++ monkey/src/projectsmanager/ui/UIAddExistingFiles.h
@@ -0,0 +1,44 @@
+/****************************************************************************
+**
+** 		Created using Monkey Studio v1.7.0
+** Author    : Azevedo Filipe aka Nox P@sNox <pasnox@gmail.com>
+** Project   : UIAddExistingFiles
+** FileName  : UIAddExistingFiles.h
+** Date      : 2007-10-13T20:00:33
+** License   : GPL
+** Comment   : Your comment here
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+#ifndef UIADDEXISTINGFILES_H
+#define UIADDEXISTINGFILES_H
+
+#include <QFileDialog>
+#include <QGroupBox>
+
+#include "ProjectsModel.h"
+#include "ProjectsProxy.h"
+#include "ProjectItem.h"
+#include "pTreeComboBox.h"
+
+class UIAddExistingFiles : public QFileDialog
+{
+	Q_OBJECT
+
+public:
+	UIAddExistingFiles( ProjectsProxy*, ProjectItem*, const QStringList& = QStringList( "=" ), QWidget* = 0 );
+
+	inline ProjectItem* currentItem() const { return qobject_cast<ProjectsModel*>( mProxy->sourceModel() )->itemFromIndex( mProxy->mapToSource( tcbProjects->currentIndex() ) ); }
+	inline QString currentOperator() const { return cbOperators->currentText(); }
+
+protected:
+	ProjectsProxy* mProxy;
+	ProjectItem* mItem;
+	pTreeComboBox* tcbProjects;
+	QComboBox* cbOperators;
+
+};
+
+#endif // UIADDEXISTINGFILES_H
