07291514012a970cb3d378db1780a0bfde43dc0f
Filipe Azevedo <pasnox@gmail.com>
2007-10-15 09:10:09 +0000

    Fix build, addProject now take scope and operator

--- monkey/src/templatesmanager/ui/UITemplatesWizard.cpp
+++ monkey/src/templatesmanager/ui/UITemplatesWizard.cpp
@@ -229,7 +229,7 @@ void UITemplatesWizard::accept()
 				{
 					if ( t == pTemplate::ttProjects && i == 0 )
 					{
-						if ( !it->project()->addProject( s ) )
+						if ( !it->project()->addProject( s, it, cbOperators->currentText() ) )
 							warning( "Adding Project...", tr( "Can't add project:\n%1" ).arg( s ) );
 					}
 					else if ( t != pTemplate::ttProjects )
