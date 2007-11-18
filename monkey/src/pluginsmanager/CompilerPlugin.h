5759c88b93da3353f717f467928a7934c1b116b0
Filipe Azevedo <pasnox@gmail.com>
2007-11-18 15:50:36 +0000

    enhance plugin interface

--- monkey/src/pluginsmanager/CompilerPlugin.h
+++ monkey/src/pluginsmanager/CompilerPlugin.h
@@ -3,10 +3,16 @@
 
 #include "MonkeyExport.h"
 #include "BasePlugin.h"
+#include "pCommand.h"
+#include "UICompilerSettings.h"
 
 class Q_MONKEY_EXPORT CompilerPlugin : public BasePlugin
 {
 public:
+	virtual pCommand defaultCompileCommand() const = 0;
+	virtual pCommand compileCommand() const = 0;
+	virtual void setCompileCommand( const pCommand& ) = 0;
+	virtual QWidget* compilerSettingsWidget() { return new UICompilerSettings( this, QApplication::activeWindow() ); }
 	// initialize compiler configuration getting parameter, and sending string configuration
 	virtual QString setUp( const QString& ) const = 0;
 
