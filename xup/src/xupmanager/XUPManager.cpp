aad9d16183c4dd10c49691d3cef3ce3a67b4fd36
Filipe Azevedo <pasnox@gmail.com>
2008-01-23 00:26:17 +0000

    move many code to ProjectItem

--- xup/src/xupmanager/XUPManager.cpp
+++ xup/src/xupmanager/XUPManager.cpp
@@ -3,11 +3,8 @@
 
 #include <QFile>
 
-QStringList mTextTypes = QStringList() << "comment" << "value" << "emptyline" << "function";
-QStringList mFileVariables = QStringList();
-QStringList mPathVariables = QStringList();
-QHash<QString, QString> mVariableLabels = QHash<QString, QString>();
-QHash<QString, QIcon> mVariableIcons = QHash<QString, QIcon>();
+QString tr( const char* s, const char* c = 0, int n = -1 )
+{ return QObject::tr( s, c, n ); }
 
 bool XUPManager::loadXUP( ProjectItem* pi, const QByteArray& a, const QString& v )
 {
@@ -22,7 +19,8 @@ bool XUPManager::loadXUP( ProjectItem* pi, const QByteArray& a, const QString& v
 	if ( e.isNull() )
 		return false;
 	// set project document and element
-	pi->setDomElement( e.cloneNode( false ).toElement() );
+	pi->setDomDocument( d );
+	pi->setDomElement( e );
 	pi->setValue( "version", v );
 	// parse project scope
 	parseRoot( pi, e );
@@ -57,7 +55,7 @@ bool XUPManager::saveXUP( ProjectItem* pi, const QString& s, const QString& v )
 	// set project version
 	pi->setValue( "version", v );
 	// write content
-	return f.write( pi->toDomDocument().toByteArray() ) != -1;
+	return f.write( pi->domDocument().toByteArray() ) != -1;
 }
 
 void XUPManager::parseRoot( ProjectItem* p, QDomElement e )
@@ -77,136 +75,9 @@ void XUPManager::parseRoot( ProjectItem* p, QDomElement e )
 			// append item to parent
 			p->appendRow( it );
 			// assign node
-			it->setDomElement( de.cloneNode( textTypes().contains( de.tagName() ) ).toElement() );
+			it->setDomElement( de );
 			// parse scope
 			parseRoot( it, de );
 		}
-	}
-}
-
-void XUPManager::recursiveNodes( QDomNode& n, ProjectItem* it )
-{
-	QDomNode dn = n.appendChild( it->domElement() );
-	foreach ( ProjectItem* cit, it->children( false, true ) )
-		recursiveNodes( dn, dynamic_cast<ProjectItem*>( cit ) );
-}
-
-QDomDocument XUPManager::toDomDocument( ProjectItem* it )
-{
-	QDomDocument d( "XUPProject" );
-	recursiveNodes( d, it );
-	return d;
-}
-
-QStringList XUPManager::textTypes()
-{ return mTextTypes; }
-
-void XUPManager::registerTextType( const QString& s )
-{
-	if ( !mTextTypes.contains( s ) )
-		mTextTypes << s;
-}
-
-QStringList XUPManager::fileVariables()
-{ return mFileVariables; }
-
-void XUPManager::registerFileVariables( const QString& s )
-{
-	if ( !mFileVariables.contains( s ) )
-		mFileVariables << s;
-}
-
-QStringList XUPManager::pathVariables()
-{ return mPathVariables; }
-
-void XUPManager::registerPathVariables( const QString& s )
-{
-	if ( !mPathVariables.contains( s ) )
-		mPathVariables << s;
-}
-
-QHash<QString, QString> XUPManager::variableLabels()
-{ return mVariableLabels; }
-
-void XUPManager::registerVariableLabels( const QString& v, const QString& l )
-{ mVariableLabels[v] = l; }
-
-QHash<QString, QIcon> XUPManager::variableIcons()
-{ return mVariableIcons; }
-
-void XUPManager::registerVariableIcons( const QString& v, const QIcon& i )
-{ mVariableIcons[v] = i; }
-
-QIcon XUPManager::getIcon( const QString& o, const QString& d )
-{ return QIcon( QFile::exists( o ) ? o : QString( ":/icons/icons/%1.png" ).arg( d ) ); }
-
-QString XUPManager::valueName( const QString& s )
-{
-	if ( mTextTypes.contains( s ) )
-		return "text";
-	return "name";
-}
-
-void XUPManager::updateItem( ProjectItem* it )
-{
-	// set icon
-	it->setIcon( getIcon( it->value( "icon" ), it->value( "type" ) ) );
-	// set caption
-	it->setText( it->defaultValue() );
-	// get element tagname
-	QString tn = it->value( "type" );
-	// set visual datas
-	if ( tn == "project" )
-	{
-		// set ToolTip
-		it->setToolTip( QString( "<b>Project</b><br />Name : %1<br />File Path : %2" ).arg( it->defaultValue() ).arg( it->projectFilePath() ) );
-	}
-	else if ( tn == "comment" )
-	{
-		// set ToolTip
-		it->setToolTip( QString( "<b>Comment</b><br />%1" ).arg( it->defaultValue() ) );
-	}
-	else if ( tn == "emptyline" )
-	{
-		// set caption
-		it->setText( QString( "%1 Empty Line(s)" ).arg( it->defaultValue() ) );
-		// set ToolTip
-		it->setToolTip( QString( "<b>Empty Line(s)</b><br />Count : %1" ).arg( it->defaultValue() ) );
-	}
-	else if ( tn == "variable" )
-	{
-		// set icon
-		QIcon i = QFile::exists( it->value( "icon" ) ) ? QIcon( it->value( "icon" ) ) : variableIcons().value( it->value( "name" ) );
-		if ( i.isNull() )
-			i = getIcon( QString(), it->value( "type" ) );
-		it->setIcon( i );
-		// set caption
-		if ( variableLabels().contains( it->defaultValue() ) )
-			it->setText( variableLabels().value( it->defaultValue() ) );
-		// set ToolTip
-		it->setToolTip( QString( "<b>Variable</b><br />Name : %1<br />Operator : %2<br />MultiLine : %3" ).arg( it->defaultValue() ).arg( it->value( "operator", "=" ) ).arg( QVariant( it->value( "multiline", "false" ) ).toBool() ? "True" : "False" ) );
-	}
-	else if ( tn == "value" )
-	{
-		// set caption
-		if ( it->parent() && fileVariables().contains( it->parent()->defaultValue() ) )
-			it->setText( QFileInfo( it->defaultValue() ).fileName() );
-		// set ToolTip
-		it->setToolTip( QString( "<b>Value</b><br />Content : %1<br />Comment : %2" ).arg( it->defaultValue() ).arg( it->value( "comment" ) ) );
-	}
-	else if ( tn == "function" )
-	{
-		// set ToolTip
-		it->setToolTip( QString( "<b>Function</b><br />Content : %1<br />Comment : %2" ).arg( it->defaultValue() ).arg( it->value( "comment" ) ) );
-	}
-	else if ( tn == "scope" )
-	{
-		// set ToolTip
-		it->setToolTip( QString( "<b>Scope</b><br />Name : %1<br />Nested : %2" ).arg( it->defaultValue() ).arg( QVariant( it->value( "nested", "false" ) ).toBool() ? "True" : "False" ) );
-	}
-	else if ( tn == "folder" )
-	{
-		// set ToolTip
-		it->setToolTip( QString( "<b>Folder</b><br />Name : %1" ).arg( it->defaultValue() ) );
 	}
 }
--- xup/src/xupmanager/XUPManager.h
+++ xup/src/xupmanager/XUPManager.h
@@ -2,8 +2,6 @@
 #define XUPMANAGER_H
 
 #include <QtXml>
-#include <QIcon>
-#include <QHash>
 
 class ProjectItem;
 
@@ -13,28 +11,6 @@ namespace XUPManager
 	bool loadXUP( ProjectItem*, const QString&, const QString& = QString( "1.0.0" ) );
 	bool saveXUP( ProjectItem*, const QString&, const QString& = QString( "1.0.0" ) );
 	void parseRoot( ProjectItem*, QDomElement );
-	
-	void recursiveNodes( QDomNode&, ProjectItem* );
-	QDomDocument toDomDocument( ProjectItem* );
-	
-	QStringList textTypes();
-	void registerTextType( const QString& );
-	
-	QStringList fileVariables();
-	void registerFileVariables( const QString& );
-	
-	QStringList pathVariables();
-	void registerPathVariables( const QString& );
-	
-	QHash<QString, QString> variableLabels();
-	void registerVariableLabels( const QString&, const QString& );
-	
-	QHash<QString, QIcon> variableIcons();
-	void registerVariableIcons( const QString&, const QIcon& );
-	
-	QIcon getIcon( const QString&, const QString& );
-	QString valueName( const QString& );
-	void updateItem( ProjectItem* );
 };
 
 #endif // XUPMANAGER_H
