0d77fbd30aa9d5b895ed83226b137208420366c0
Filipe Azevedo <pasnox@gmail.com>
2008-03-25 20:36:50 +0000

    Add sdk files

---
+++ monkey/sdk/projectsmanager.h
@@ -0,0 +1,6 @@
+#include "../src/projectsmanager/ProjectItem.h"
+#include "../src/projectsmanager/ProjectsModel.h"
+#include "../src/projectsmanager/ProjectsProxy.h"
+
+#include "../src/projectsmanager/ui/UIAddExistingFiles.h"
+#include "../src/projectsmanager/ui/UIProjectsManager.h"
---
+++ monkey/sdk/settingsmanager.h
@@ -0,0 +1,1 @@
+#include "../src/settingsmanager/Settings.h"
