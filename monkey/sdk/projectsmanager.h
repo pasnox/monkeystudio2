ff4ffa5c1bd4aa3410671dbb885ef9c78c201e79
Filipe Azevedo <pasnox@gmail.com>
2008-03-30 15:13:48 +0000

    repalce projectsmanager by xupmanager

--- monkey/sdk/projectsmanager.h
+++
@@ -1,6 +0,0 @@
-#include "../src/projectsmanager/ProjectItem.h"
-#include "../src/projectsmanager/ProjectsModel.h"
-#include "../src/projectsmanager/ProjectsProxy.h"
-
-#include "../src/projectsmanager/ui/UIAddExistingFiles.h"
-#include "../src/projectsmanager/ui/UIProjectsManager.h"
