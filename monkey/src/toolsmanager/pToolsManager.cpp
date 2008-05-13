4b4c37595b327fc465d2085f77d2c9ae3552f07e
Filipe Azevedo <pasnox@gmail.com>
2008-05-13 21:52:44 +0000

    Fix bug that crash monkey studio when adding a tool and not tweaking it and trying execute it ( empty file path )

--- monkey/src/toolsmanager/pToolsManager.cpp
+++ monkey/src/toolsmanager/pToolsManager.cpp
@@ -9,21 +9,21 @@
 ** Comment   : This header has been automatically generated, if you are the original author, or co-author, fill free to replace/append with your informations.
 ** Home Page : http://www.monkeystudio.org
 **
-    Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
+	Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **
 ****************************************************************************/
 #include "pToolsManager.h"
@@ -119,7 +119,9 @@ void pToolsManager::toolsMenu_triggered( QAction* a )
 	QString t = cm->processInternalVariables( a->statusTip() );
 	QString w = cm->processInternalVariables( a->data().toString() );
 	bool b = false;
-	if ( w.isEmpty() && QFile::exists( t ) )
+	if ( t.isEmpty() )
+		b = false;
+	else if ( w.isEmpty() && QFile::exists( t ) )
 		b = QDesktopServices::openUrl( QUrl::fromLocalFile( t ) );
 	else if ( w.isEmpty() )
 		b = QProcess::startDetached( t );
@@ -132,5 +134,5 @@ void pToolsManager::toolsMenu_triggered( QAction* a )
 		b = p->waitForStarted();
 	}
 	if ( !b )
-		warning( tr( "Tools Error..." ), tr( "Error trying to start tool :\n%1" ).arg( t ) );
+		warning( tr( "Tools Error..." ), tr( "Error trying to start tool :\n'%1'" ).arg( t ) );
 }
