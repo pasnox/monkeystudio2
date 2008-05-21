aa51c3ba392994cf900a06e87240a34163377c9b
Filipe Azevedo <pasnox@gmail.com>
2008-05-21 23:13:58 +0000

    Update C++ templates

--- templates/C++/Class/$Base file name$.h
+++ templates/C++/Class/$Base file name$.h
@@ -5,8 +5,14 @@ class $Class name$
 {
 public:
 	$Class name$();
-	~$Class name$();
+	virtual ~$Class name$();
 
+	/*** For dynamic memory management (can be removed if it isn't necessary) ***/
+	$Class name$(const $Class name$ & obj);
+	$Class name$ & operator =(const $Class name$ & obj);
+	bool operator ==(const $Class name$ & obj) const;
+	bool operator !=(const $Class name$ & obj) const;
+	/*** End of dynamic memory management ***/
 };
 
 #endif // $Base file name.upper$_H
