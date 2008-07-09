849cf9d661435f19eb4921ea0c88d01709aeac80
Filipe Azevedo <pasnox@gmail.com>
2008-07-09 19:59:43 +0000

    Documents code using doxygen style

--- fresh/widgets/pAction.cpp
+++ fresh/widgets/pAction.cpp
@@ -1,30 +1,19 @@
 /****************************************************************************
-**
-** 		Created using Monkey Studio v1.8.1.0
-** Authors    : Filipe AZEVEDO aka Nox P@sNox <pasnox@gmail.com>
-** Project   : Fresh Framework
-** FileName  : pAction.cpp
-** Date      : 2008-01-14T00:27:38
-** License   : GPL
-** Comment   : This header has been automatically generated, if you are the original author, or co-author, fill free to replace/append with your informations.
-** Home Page : http://www.monkeystudio.org
-**
-    Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
+	Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-**
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ****************************************************************************/
 #include "pAction.h"
 
@@ -32,44 +21,59 @@ const char* pAction::_DEFAULT_SHORTCUT_PROPERTY_ = "Default Shortcut";
 const char* pAction::_GROUP_PROPERTY_ = "Group";
 const char* pAction::_SETTINGS_SCOPE_ = "Shortcuts Manager";
 
-pAction::pAction( const QString& n, const QString& s, const QKeySequence& sc, const QString& g )
+/*!
+	\brief Construct a new pAction
+	\param name The action internal name
+	\param text The action text
+	\param keySequence The action shortcut
+	\param group The action group
+*/
+pAction::pAction( const QString& name, const QString& text, const QKeySequence& keySequence, const QString& group )
 	: QAction( pActionManager::instance() )
 {
 	// set object name
-	setObjectName( n );
+	setObjectName( name );
 
 	// add to group
-	pActionManager::addAction( g, this );
+	pActionManager::addAction( group, this );
 
 	// set action text
-	setText( s );
+	setText( text );
 
 	// set action shortcut
-	setProperty( _DEFAULT_SHORTCUT_PROPERTY_, sc.toString() );
-	setShortcut( pActionManager::getShortcut( g, this, sc ) );
+	setProperty( _DEFAULT_SHORTCUT_PROPERTY_, keySequence.toString() );
+	setShortcut( pActionManager::getShortcut( group, this, keySequence ) );
 }
 
-pAction::pAction( const QString& n, const QIcon& i, const QString& s, const QKeySequence& sc, const QString& g )
+/*!
+	\brief Construct a new pAction
+	\param name The action internal name
+	\param icon The action icon
+	\param text The action text
+	\param keySequence The action shortcut
+	\param group The action group
+*/
+pAction::pAction( const QString& name, const QIcon& icon, const QString& text, const QKeySequence& keySequence, const QString& group )
 	: QAction( pActionManager::instance() )
 {
 	// set object name
-	setObjectName( n );
+	setObjectName( name );
 
 	// fixed group
-	QString cg = fixedString( g );
+	QString cg = fixedString( group );
 
 	// add to group
 	pActionManager::addAction( cg, this );
 
 	// set icon
-	setIcon( i );
+	setIcon( icon );
 
 	// set action text
-	setText( s );
+	setText( text );
 
 	// set action shortcut
-	setProperty( _DEFAULT_SHORTCUT_PROPERTY_, sc.toString() );
-	setShortcut( pActionManager::getShortcut( cg, this, sc ) );
+	setProperty( _DEFAULT_SHORTCUT_PROPERTY_, keySequence.toString() );
+	setShortcut( pActionManager::getShortcut( cg, this, keySequence ) );
 }
 
 pAction::~pAction()
@@ -78,9 +82,13 @@ pAction::~pAction()
 	pActionManager::removeAction( property( _GROUP_PROPERTY_ ).toString(), this );
 }
 
-QString pAction::fixedString( const QString& s )
+/*!
+	\details Return a string that is proper to show
+	\param string The string to fix
+*/
+QString pAction::fixedString( const QString& string )
 {
-	QString p = s;
+	QString p = string;
 	
 	// remove trailing /
 	if ( p.endsWith( "/" ) )
--- fresh/widgets/pActionManager.cpp
+++ fresh/widgets/pActionManager.cpp
@@ -1,30 +1,19 @@
 /****************************************************************************
-**
-** 		Created using Monkey Studio v1.8.1.0
-** Authors    : Filipe AZEVEDO aka Nox P@sNox <pasnox@gmail.com>
-** Project   : Fresh Framework
-** FileName  : pActionManager.cpp
-** Date      : 2008-01-14T00:27:39
-** License   : GPL
-** Comment   : This header has been automatically generated, if you are the original author, or co-author, fill free to replace/append with your informations.
-** Home Page : http://www.monkeystudio.org
-**
-    Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
+	Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-**
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ****************************************************************************/
 #include "pActionManager.h"
 #include "pAction.h"
@@ -33,22 +22,36 @@
 
 #include <QApplication>
 
-pActionManager::pActionManager( QObject* o )
-	: QObject( o ), mError( QString() ), mSettings( 0 )
+/*!
+	\details Create an action manager
+	\param parent The parent object
+*/
+pActionManager::pActionManager( QObject* parent )
+	: QObject( parent ), mError( QString() ), mSettings( 0 )
 {}
 
-void pActionManager::setSettings( pSettings* s )
+/*!
+	\details Set the settings object used by this action manager
+	\param settings The settings object
+*/
+void pActionManager::setSettings( pSettings* settings )
 {
-	if ( instance()->mSettings != s )
+	if ( instance()->mSettings != settings )
 	{
-		instance()->mSettings = s;
+		instance()->mSettings = settings;
 		instance()->reloadSettings();
 	}
 }
 
+/*!
+	\details Return the settings object used by this action manager, can be null
+*/
 pSettings* pActionManager::settings()
 { return instance()->mSettings; }
 
+/*!
+	\details Reload all action properties
+*/
 void pActionManager::reloadSettings()
 {
 	// restore shortcut for each action
@@ -62,55 +65,85 @@ void pActionManager::reloadSettings()
 	}
 }
 
-QAction* pActionManager::addAction( const QString& g, QAction* a )
+/*!
+	\details Add an action to a group
+	\param group The group to add the action to
+	\param action The action to add
+	\return The added action
+*/
+QAction* pActionManager::addAction( const QString& group, QAction* action )
 {
 	// need group
-	Q_ASSERT( !g.isEmpty() );
+	Q_ASSERT( !group.isEmpty() );
 	// need action
-	Q_ASSERT( a );
+	Q_ASSERT( action );
 	// action name is require
-	Q_ASSERT( !a->objectName().isEmpty() );
+	Q_ASSERT( !action->objectName().isEmpty() );
 	// addactions to group if it donesn't contains already
-	if ( !actions()[g].contains( a ) )
+	if ( !actions()[group].contains( action ) )
 	{
-		a->setProperty( pAction::_GROUP_PROPERTY_, g );
-		instance()->mActions[g] << a;
+		action->setProperty( pAction::_GROUP_PROPERTY_, group );
+		instance()->mActions[group] << action;
 	}
 	// return action
-	return a;
+	return action;
 }
 
-void pActionManager::removeAction( const QString& g, QAction* a )
+/*!
+	\details Remove an action from a group
+	\param group The group to remove the action of
+	\param action The action to remove
+*/
+void pActionManager::removeAction( const QString& group, QAction* action )
 {
 	// need group
-	Q_ASSERT( !g.isEmpty() );
+	Q_ASSERT( !group.isEmpty() );
 	// need action
-	Q_ASSERT( a );
+	Q_ASSERT( action );
 	// action name is require
-	Q_ASSERT( !a->objectName().isEmpty() );
+	Q_ASSERT( !action->objectName().isEmpty() );
 	// addactions to group if it donesn't contains already
-	if ( actions()[g].contains( a ) )
-		instance()->mActions[g].removeAll( a );
+	if ( actions()[group].contains( action ) )
+		instance()->mActions[group].removeAll( action );
 }
 
+/*!
+	\details Return the hash actions container
+*/
 pHashActionList pActionManager::actions()
 { return instance()->mActions; }
 
+/*!
+	\details Return the default global group
+*/
 QString pActionManager::globalGroup()
 { return tr( "Global" ); }
 
-QKeySequence pActionManager::getShortcut( const QString& g, QAction* a, const QKeySequence& sc )
+/*!
+	\details Return an action key sequence from settings or the default one.
+	\details Return a null key sequence if action or group is invalid.
+	\param group The action group
+	\param action The action
+	\param  keySequence The default key sequence returned if the settings don't contains entry for this group/action
+*/
+QKeySequence pActionManager::getShortcut( const QString& group, QAction* action, const QKeySequence& keySequence )
 {
 	// need group
-	Q_ASSERT( !g.isEmpty() );
+	Q_ASSERT( !group.isEmpty() );
 	// need action
-	Q_ASSERT( a != 0 );
+	Q_ASSERT( action != 0 );
 	// return defined shortcuts or the default one
-	pSettings* s = instance()->settings();
-	return s ? QKeySequence( s->value( QString( "%1/%2/%3" ).arg( pAction::_SETTINGS_SCOPE_ ).arg( g ).arg( a->objectName() ), sc ).toString() ) : QKeySequence();
+	pSettings* settings = instance()->settings();
+	return settings ? QKeySequence( settings->value( QString( "%1/%2/%3" ).arg( pAction::_SETTINGS_SCOPE_ ).arg( group ).arg( action->objectName() ), keySequence ).toString() ) : QKeySequence();
 }
 
-bool pActionManager::setShortcut( QAction* action, const QKeySequence& sc )
+/*!
+	\details Try to set a action key sequence, return true on success, else false.
+	\details If there is an error, yo ucan read it using lastError.
+	\param action The action to set the shortcut too
+	\param keySequence The key sequence to set
+*/
+bool pActionManager::setShortcut( QAction* action, const QKeySequence& keySequence )
 {
 	// need action
 	Q_ASSERT( action != 0 );
@@ -119,9 +152,9 @@ bool pActionManager::setShortcut( QAction* action, const QKeySequence& sc )
 	{
 		foreach ( QAction* a, al )
 		{
-			if ( a != action && a->shortcut() == sc && !sc.isEmpty() )
+			if ( a != action && a->shortcut() == keySequence && !keySequence.isEmpty() )
 			{
-				instance()->mError = tr( "Key Sequence '%1' already assigned to the '%2/%3'" ).arg( sc.toString() ).arg( a->property( pAction::_GROUP_PROPERTY_ ).toString() ).arg( a->text() );
+				instance()->mError = tr( "Key Sequence '%1' already assigned to the '%2/%3'" ).arg( keySequence.toString() ).arg( a->property( pAction::_GROUP_PROPERTY_ ).toString() ).arg( a->text() );
 				return false;
 			}
 		}
@@ -129,19 +162,25 @@ bool pActionManager::setShortcut( QAction* action, const QKeySequence& sc )
 	// remove old/set shortcut entry
 	if ( pSettings* s = instance()->settings() )
 	{
-		if ( action->shortcut() == sc )
+		if ( action->shortcut() == keySequence )
 			s->remove( QString( "%1/%2/%3" ).arg( pAction::_SETTINGS_SCOPE_ ).arg( action->property( pAction::_GROUP_PROPERTY_ ).toString() ).arg( action->objectName() ) );
 		else
-			s->setValue( QString( "%1/%2/%3" ).arg( pAction::_SETTINGS_SCOPE_ ).arg( action->property( pAction::_GROUP_PROPERTY_ ).toString() ).arg( action->objectName() ), sc.toString() );
+			s->setValue( QString( "%1/%2/%3" ).arg( pAction::_SETTINGS_SCOPE_ ).arg( action->property( pAction::_GROUP_PROPERTY_ ).toString() ).arg( action->objectName() ), keySequence.toString() );
 	}
 	// set new shortcut to action
-	action->setShortcut( sc );
+	action->setShortcut( keySequence );
 	// return success
 	return true;
 }
 
+/*!
+	\details Return the last error string
+*/
 QString pActionManager::lastError()
 { return instance()->mError; }
 
+/*!
+	\details Show the action key sequence editor
+*/
 void pActionManager::showSettings()
 { pShortcutsEditor::instance( QApplication::activeWindow() )->exec(); }
