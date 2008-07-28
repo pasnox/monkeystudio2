bbbbe299ea81d79b2fc53b98b73db4b94e5150e9
Filipe Azevedo <pasnox@gmail.com>
2008-07-28 20:15:24 +0000

    Little fix

--- monkey/src/variablesmanager/VariablesManager.cpp
+++ monkey/src/variablesmanager/VariablesManager.cpp
@@ -1,38 +1,20 @@
 /****************************************************************************
-**
-** 		Created using Monkey Studio v1.8.1.0
-** Authors    : Filipe AZEVEDO aka Nox P@sNox <pasnox@gmail.com>
-** Project   : Monkey Studio IDE
-** FileName  : VariablesManager.cpp
-** Date      : 2008-01-14T00:37:18
-** License   : GPL
-** Comment   : This header has been automatically generated, if you are the original author, or co-author, fill free to replace/append with your informations.
-** Home Page : http://www.monkeystudio.org
-**
-    Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
+	Copyright (C) 2005 - 2008  Filipe AZEVEDO & The Monkey Studio Team
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-**
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ****************************************************************************/
-/*!
-	\file VariablesManager.cpp
-	\date 2008-01-14T00:37:18
-	\author Andrei KOPATS
-	\brief Implementation of VariablesManager class
-*/
-
 #include "VariablesManager.h"
 
 #include <QStringList>
@@ -50,27 +32,28 @@ VariablesManager::VariablesManager( QObject* o )
 
 /*!
 	Get value of variable
+	\param name Variable name
 	\param locals Local dictionary of variables
 	\return Value of variable. Empty string, if variable is unknown
 */
-QString VariablesManager::getVariable (QString name, Dictionary locals)
+QString VariablesManager::getVariable( QString name, Dictionary locals )
 {
-    QString result = QString::null;
-    // monkeystudio_version
-    if ( name == "editor_version" )
-        result = PACKAGE_VERSION;
-    // monkeystudio_version_string
-    else if ( name == "editor_version_string" )
-        result = QString( "%1 v%2" ).arg( PACKAGE_NAME ).arg( PACKAGE_VERSION );
-    else if ( name == "date" )
-        result = QDateTime::currentDateTime().toString( Qt::ISODate );
-    if (!result.isEmpty())
-        return result;
-    if ( globals.contains(name))
-        return globals[name];
-    if (locals.contains(name))
-        return locals[name];
-    return QString::null;
+	QString result = QString::null;
+	// monkeystudio_version
+	if ( name == "editor_version" )
+		result = PACKAGE_VERSION;
+	// monkeystudio_version_string
+	else if ( name == "editor_version_string" )
+		result = QString( "%1 v%2" ).arg( PACKAGE_NAME ).arg( PACKAGE_VERSION );
+	else if ( name == "date" )
+		result = QDateTime::currentDateTime().toString( Qt::ISODate );
+	if (!result.isEmpty())
+		return result;
+	if ( globals.contains(name))
+		return globals[name];
+	if (locals.contains(name))
+		return locals[name];
+	return QString::null;
 }
 
 /*!
@@ -82,11 +65,11 @@ QString VariablesManager::getVariable (QString name, Dictionary locals)
 */
 bool VariablesManager::isSet (QString name, Dictionary& locals)
 {
-    if (    name == "editor_version" ||
-            name == "editor_version_string" ||
-            name == "date" )
-        return true;
-    return (globals.contains(name) || locals.contains(name));
+	if (    name == "editor_version" ||
+			name == "editor_version_string" ||
+			name == "date" )
+		return true;
+	return (globals.contains(name) || locals.contains(name));
 }
 
 /*!
@@ -97,20 +80,20 @@ bool VariablesManager::isSet (QString name, Dictionary& locals)
 */
 QString VariablesManager::replaceAllVariables (QString text, Dictionary locals)
 {
-    int p = 0;
-    QString s;
-    QRegExp rex( "(\\$[^$\\n]+\\$)" );
-    // search and interpret values
-    QList<QString> findedVariables;
-    while ( ( p = rex.indexIn( text, p ) ) != -1 )
-    {
-        // got keyword
-        s = rex.capturedTexts().value( 1 );
-        findedVariables.append (s);
-        p += rex.matchedLength();
-    }
-    // replace occurences
-    foreach ( QString s, findedVariables )
+	int p = 0;
+	QString s;
+	QRegExp rex( "(\\$[^$\\n]+\\$)" );
+	// search and interpret values
+	QList<QString> findedVariables;
+	while ( ( p = rex.indexIn( text, p ) ) != -1 )
+	{
+		// got keyword
+		s = rex.capturedTexts().value( 1 );
+		findedVariables.append (s);
+		p += rex.matchedLength();
+	}
+	// replace occurences
+	foreach ( QString s, findedVariables )
 	{
 		QString fuckDollar = QString(s).remove(s.size()-1,1).remove(0,1);
 		bool toup = false;
@@ -130,9 +113,9 @@ QString VariablesManager::replaceAllVariables (QString text, Dictionary locals)
 			replaceWith = replaceWith.toUpper();
 		else if (tolow)
 			replaceWith = replaceWith.toLower();
-        text.replace( s, replaceWith);
+		text.replace( s, replaceWith);
 		text.replace( "\\n", "\n");
 	}
-    // return value
-    return text;
+	// return value
+	return text;
 }
