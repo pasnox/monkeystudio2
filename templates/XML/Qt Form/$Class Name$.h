87ee17e6267467fcfabbb1cf40f19e99fbbb3217
Filipe Azevedo <pasnox@gmail.com>
2008-09-23 21:46:10 +0000

    Add new Qt Form template this one generate complete form skeleton UI, H and CPP

---
+++ templates/XML/Qt Form/$Class Name$.h
@@ -0,0 +1,15 @@
+#ifndef $Class Name.upper$_H
+#define $Class Name.upper$_H
+
+#include "ui_$Class Name$.h"
+
+class $Class Name$ : public $Qt Widget Class$, public Ui::$Class Name$
+{
+	Q_OBJECT
+
+public:
+	$Class Name$( QWidget* parent = 0 );
+	virtual ~$Class Name$();
+};
+
+#endif // $Class Name.upper$_H
