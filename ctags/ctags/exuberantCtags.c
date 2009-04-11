812fb2ea6e869284e784db3a977a92c97e9f12f9
Filipe Azevedo <pasnox@gmail.com>
2009-04-11 17:41:11 +0000

    Update ctags library

--- ctags/ctags/exuberantCtags.c
+++ ctags/ctags/exuberantCtags.c
@@ -1,40 +1,139 @@
 #include "general.h"
-#include "entry.h"
 #include "string.h"
-#include "parse.h"
 #include "read.h"
 
-extern TagEntryListItem* parseFile ( const char* fileName, const char* langName)
+extern void initCtags()
 {
-	const parserDefinition*  language;
+	initializeParsing();
+	installLanguageMapDefaults();
+}
+
+extern void deInitCtags()
+{
+	freeParserResources();
+}
+
+extern TagEntryListItem* createTagEntryListItem( const char* fileName, const char* langName )
+{
+	const parserDefinition* language;
 	langType lang; 
 	unsigned int passCount;
 	boolean retried;
 
 	firstTagEntry = NULL; // generate new list
-	if ( (langName != NULL ) && (strlen ( langName) != 0 ))
-		lang = getNamedLanguage ( langName );
-	else
-		lang = getFileLanguage ( fileName) ;
-	if ( lang  <=0)
+	
+	if ( ( langName != NULL ) && ( strlen( langName ) != 0 ) )
 	{
-		printf ( "Will not parse %s\n",fileName);
+		lang = getNamedLanguage( langName );
+	}
+	else
+	{
+		lang = getFileLanguage( fileName );
+	}
+	
+	if ( lang <= 0 )
+	{
+		printf( "Will not parse %s\n",fileName );
 		return NULL;
 	}
-	language = LanguageTable [lang];
-	if ( language == NULL)
+	
+	language = LanguageTable[ lang ];
+	
+	if ( language == NULL )
+	{
 		return NULL;
-	if (!fileOpen (fileName, lang))
+	}
+	
+	if ( !fileOpen( fileName, lang ) )
+	{
 		return NULL;
+	}
 	
 	passCount = 0;
 	retried = FALSE;
-	if (language->parser != NULL)
-		language->parser ();
-	else if (language->parser2 != NULL)
+	
+	if ( language->parser != NULL )
+	{
+		language->parser();
+	}
+	else if ( language->parser2 != NULL )
+	{
 		do
 		{
-			retried = language->parser2 (++passCount);
-		}while ( retried);
+			retried = language->parser2( ++passCount );
+		} while ( retried );
+	}
+	
 	return firstTagEntry;
+}
+
+extern void freeTagEntryListItem( TagEntryListItem* item )
+{
+	while ( item != NULL )
+	{
+		tagEntryInfo* entry = &item->tag;
+		
+		if ( entry->language )
+			free( (char*)entry->language );
+		
+		if ( entry->sourceFileName )
+			free( (char*)entry->sourceFileName );
+		
+		if ( entry->name)
+			free( (char*)entry->name );
+		
+		if ( entry->kindName )
+			free( (char*)entry->kindName );
+		
+		if ( entry->extensionFields.access )
+			free( (char*)entry->extensionFields.access );
+		
+		if ( entry->extensionFields.fileScope )
+			free( (char*)entry->extensionFields.fileScope );
+		
+		if ( entry->extensionFields.implementation )
+			free( (char*)entry->extensionFields.implementation  );
+		
+		if ( entry->extensionFields.inheritance )
+			free( (char*)entry->extensionFields.inheritance );
+		
+		if ( entry->extensionFields.scope[0] )
+			free( (char*)entry->extensionFields.scope[0] );
+		
+		if ( entry->extensionFields.scope[1] )
+			free( (char*)entry->extensionFields.scope[1] );
+		
+		if ( entry->extensionFields.typeRef[0] )
+			free( (char*)entry->extensionFields.typeRef[0] );
+		
+		if ( entry->extensionFields.typeRef[1] )
+			free( (char*)entry->extensionFields.typeRef[1] );
+		
+		if ( entry->extensionFields.signature )
+			free( (char*)entry->extensionFields.signature );
+		
+		TagEntryListItem* temp = item->next;
+		free( item );
+		item = temp;
+	}
+}
+
+extern void setLanguageTypeKinds( const langType language, const char* kinds )
+{
+	parserDefinition* parser = LanguageTable[ language ];
+	
+	if ( parser != NULL )
+	{
+		unsigned int i;
+		
+		for ( i = 0; i < parser->kindCount; ++i )
+		{
+			parser->kinds[ i ].enabled = strchr( kinds, parser->kinds[ i ].letter ) != NULL;
+		}
+	}
+}
+
+extern void setLanguageKinds( const char *const language, const char* kinds )
+{
+	setLanguageTypeKinds( getNamedLanguage( language ), kinds );
 }
--- ctags/ctags/exuberantCtags.h
+++ ctags/ctags/exuberantCtags.h
@@ -1,6 +1,12 @@
 #include "entry.h"
+#include "parse.h"
 
 /*procedure will return list for  file. Every time new list will be generated. For every generated list memory must be freed using Ctags::freeTagEntryList
 */
 
-extern TagEntryListItem* parseFile ( const char* fileName, const char* langName);
+extern void initCtags();
+extern void deInitCtags();
+extern TagEntryListItem* createTagEntryListItem( const char* fileName, const char* langName );
+extern void freeTagEntryListItem( TagEntryListItem* item );
+extern void setLanguageTypeKinds( const langType language, const char* kinds );
+extern void setLanguageKinds( const char* const language, const char* kinds );
