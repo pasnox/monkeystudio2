9185a72ddf7103bf8b4629ff564bf2a79807244e
Filipe Azevedo <pasnox@gmail.com>
2009-11-02 22:19:26 +0000

    Add "MkS Base Plugin" template for allow user to create custom plugins for mks ! ( others types will comes shortly ) Fix a bug in the templates manager that overwrite files that does not change in the process fixing a bug that cause images to be corrupted. Add new template icon ( mks ).

---
+++ datas/templates/C++/MkS Base Plugin/$Sources Path$$Plugin Name$.cpp
@@ -0,0 +1,28 @@
+#include "$Plugin Name$.h"
+
+void $Plugin Name$::fillPluginInfos()
+{
+	mPluginInfos.Caption = tr( "$Plugin Name$" );
+	mPluginInfos.Description = tr( "$Description$" );
+	mPluginInfos.Author = "$Authors$";
+	mPluginInfos.Type = BasePlugin::iBase;
+	mPluginInfos.Name = PLUGIN_NAME;
+	mPluginInfos.Version = "0.5.0";
+	mPluginInfos.FirstStartEnabled = false;
+	mPluginInfos.HaveSettingsWidget = false;
+	mPluginInfos.Pixmap = pIconManager::pixmap( "$Plugin Name$.png", ":/icons" );
+}
+
+bool $Plugin Name$::install()
+{
+	// place your init code here
+	return false;
+}
+
+bool $Plugin Name$::uninstall()
+{
+	// place your deinit code here
+	return false;
+}
+
+Q_EXPORT_PLUGIN2( Base$Plugin Name$, $Plugin Name$ )
