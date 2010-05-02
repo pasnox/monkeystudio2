c73caa86c4aeec959a03f708171eb13424fb29ea
Filipe Azevedo <pasnox@gmail.com>
2010-05-02 21:43:08 +0000

    Fix translations files properties.

--- datas/translations/monkeystudio_be.ts
+++ datas/translations/monkeystudio_be.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="be">
+<TS version="2.0" language="be" sourcelanguage="en">
 <context>
     <name>BasePlugin</name>
     <message>
--- datas/translations/monkeystudio_es.ts
+++ datas/translations/monkeystudio_es.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.0" language="es" sourcelanguage="en">
 <context>
     <name>BasePlugin</name>
     <message>
