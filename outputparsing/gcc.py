94b951d2e2499d93abf8fc17886e27b677812eab
Andrei Kopats <hlamer@users.noreply.github.com>
2010-09-14 13:38:40 +0000

    Fixed bug #589846

--- outputparsing/gcc.py
+++ outputparsing/gcc.py
@@ -7,7 +7,8 @@ from string import Template
 
 file_name = r"[\w\d\./\\\-]+"
 number = "\d+"
-compiler = "(gcc|g\+\+|mingw32-gcc|mingw32-g\+\+)[\-\d\.]*" #on Mac version could be included, such as g++-4.0
+#compiler = Template("(gcc|g\+\+|mingw32-gcc|mingw32-g\+\+)[\-\d\.]*") # /usr/local/i386-mingw32-3.4.5/bin/i386-mingw32-gcc-4.0
+compiler = Template("($file_name)?(gcc|g\+\+|mingw32-gcc|mingw32-g\+\+)[\-\d\.]*").substitute(file_name=file_name)
 source_file = file_name + "\.[cpmh]{1,3}" # .cpp .c .h .m .mm
 
 ## Error
@@ -111,8 +112,8 @@ link_failed.test('collect2: ld returned 1 exit status\n',
 regEx = Template("^$compiler[^\\n]+ ($source_file)")
 compiling =parsing.Pattern(regEx.substitute(compiler=compiler, source_file=source_file),
 										   type = 'compiling',
-										   text = 'Compiling %2...',
-										   file = '%2')
+										   text = 'Compiling %3...',
+										   file = '%3')
 
 compiling.setComment('Compiling')
 
@@ -136,6 +137,19 @@ compiling.test(text,
 					text = 'Compiling src/MSVCMake.cpp...',
 					hint = text[:-1],
 					file = 'src/MSVCMake.cpp')
+text = \
+'/usr/local/i386-mingw32-3.4.5/bin/i386-mingw32-gcc -c -O2 -Wall -DUNICODE -DQT_LARGEFILE_SUPPORT -DLARGE_FILES '+ \
+'-D_FILE_OFFSET_BITS=64 -DQT_NO_DEBUG -DQT_GUI_LIB -DQT_CORE_LIB -DQT_THREAD_SUPPORT -DQT_NO_DYNAMIC_CAST ' + \
+'-I"/usr/local/Trolltech/win32/4.6.0/include/QtCore" -I"/usr/local/Trolltech/win32/4.6.0/include/QtGui" ' + \
+'-I"/usr/local/Trolltech/win32/4.6.0/include" -I"." -I"/Users/pasnox/Win32Libraries/include" -I"libwbfs" ' + \
+'-I"/usr/local/Trolltech/win32/4.6.0/include/ActiveQt" -I"../build/release/moc" ' + \
+'-I"/usr/local/i386-mingw32-3.4.5/i386-mingw32/include" -I"../../../intuisphere/trunk/mkspecs/4.6.x/win32-osx-g++" ' + \
+'-o ../build/release/obj/win32/tools.o tools.c\n'
+compiling.test(text,
+					type = 'compiling',
+					text = 'Compiling tools.c...',
+					hint = text[:-1],
+					file = 'tools.c')
 #g++
 text = "g++ -c -pipe -Wall -g -Wall -W -D_REENTRANT -DQT_XML_LIB -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED -I/usr/share/qt4/mkspecs/linux-g++ -I. -I/usr/include/qt4/QtCore -I/usr/include/qt4/QtGui -I/usr/include/qt4/QtXml -I/usr/include/qt4 -Ieditor-src -Ieditor-src/branche1 -Ieditor-src -I/branche2 -Ibuild/debug/.moc -Ibuild/debug/.ui -o build/debug/.obj/unix/SourceFile.o editor-src/branche2/SourceFile.cpp"
 compiling.test(text,
