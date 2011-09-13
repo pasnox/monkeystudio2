251090fd475e23f87732d47941985b496b8c5e02
Filipe Azevedo <pasnox@gmail.com>
2011-09-13 22:48:56 +0000

    Update templates step 5

--- datas/templates/Python/Qt Form/$Class Name$.py
+++ datas/templates/Python/Qt Form/$Class Name$.py
@@ -1,14 +1,14 @@
 from PyQt4 import uic
 
-(Ui_$Class Name$, $Qt Widget Class$) = uic.loadUiType('$Class Name$.ui')
+( Ui_$Class Name$, $Qt Widget Class$ ) = uic.loadUiType( '$Class Name$.ui' )
 
-class $Class Name$ ($Qt Widget Class$):
+class $Class Name$ ( $Qt Widget Class$ ):
     """$Class Name$ inherits $Qt Widget Class$"""
 
-    def __init__ (self, parent = None):
-        $Qt Widget Class$.__init__(self, parent)
+    def __init__ ( self, parent = None ):
+        $Qt Widget Class$.__init__( self, parent )
         self.ui = Ui_$Class Name$()
-        self.ui.setupUi(self)
+        self.ui.setupUi( self )
 
-    def __del__ (self):
+    def __del__ ( self ):
         self.ui = None
