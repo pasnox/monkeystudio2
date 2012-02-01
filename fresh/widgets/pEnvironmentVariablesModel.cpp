1658c10ac3ff9ac89af233065e1a67845625cc72
Filipe Azevedo <pasnox@gmail.com>
2012-02-01 21:04:13 +0000

    Merging using first revision at 4343 and second at HEAD from branches/dev to trunk

--- fresh/widgets/pEnvironmentVariablesModel.cpp
+++ fresh/widgets/pEnvironmentVariablesModel.cpp
@@ -210,12 +210,12 @@ pEnvironmentVariablesModel::Variables pEnvironmentVariablesModel::stringListToVa
         const QString name = variable.section( '=', 0, 0 );
         const QString value = variable.section( '=', 1 );
 
-        pEnvironmentVariablesModel::Variable variable;
-        variable.name = name;
-        variable.value = value;
-        variable.enabled = true;
+        pEnvironmentVariablesModel::Variable item;
+        item.name = name;
+        item.value = value;
+        item.enabled = true;
 
-        items[ name ] = variable;
+        items[ name ] = item;
     }
     
     return items;
